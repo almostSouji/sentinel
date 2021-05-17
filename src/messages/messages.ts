1571cba8758867aa2d4c9f573b895ff91523de77
almostSouji <timoqueezle@gmail.com>
2021-05-18 00:35:25 +0200

    chore: reword dismiss button and log

--- src/messages/messages.ts
+++ src/messages/messages.ts
@@ -3,6 +3,6 @@ export const BAN_SUCCESS = (executor: string, target: string) => `• \`${execut
 export const BAN_FAIL = (executor: string, target: string) => `• \`${executor}\` could not ban \`${target}\``;
 export const DELETE_SUCCESS = (executor: string) => `•\`${executor}\` deleted the message`;
 export const DELETE_FAIL = (executor: string) => `•\`${executor}\` could not delete the message`;
-export const DISMISSED = (executor: string) => `•\`${executor}\` dismissed buttons`;
+export const DISMISSED = (executor: string) => `•\`${executor}\` reviewed this case`;
 export const APPROVED = (executor: string) => `•\`${executor}\` approved this message`;
 export const LOG_FOOTER_TEXT = (executor: string, id: string) => `Last action by ${executor} (${id})`;
