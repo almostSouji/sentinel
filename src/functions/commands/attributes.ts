06e53f6c0d969910aed0a34a0a51e14be81814c8
almostSouji <timoqueezle@gmail.com>
2021-06-20 00:09:15 +0200

    feat(command): fallback if no attributes are changed show

--- src/functions/commands/attributes.ts
+++ src/functions/commands/attributes.ts
@@ -1,12 +1,18 @@
 import { CommandInteraction } from 'discord.js';
 import { ATTRIBUTES } from '../../keys';
-import { CONFIG_ATTRIBUTES_ENABLED, CONFIG_ATTRIBUTES_DISABLED, NOT_IN_DM } from '../../messages/messages';
+import {
+	CONFIG_ATTRIBUTES_ENABLED,
+	CONFIG_ATTRIBUTES_DISABLED,
+	NOT_IN_DM,
+	CONFIG_SHOW_ATTRIBUTES,
+	CONFIG_SHOW_ATTRIBUTES_NONE,
+} from '../../messages/messages';
 
 function formatFlag(flag: string): string {
 	return flag.replaceAll('-', '_').toUpperCase();
 }
 
-export function attributes(interaction: CommandInteraction) {
+export async function attributes(interaction: CommandInteraction) {
 	const messageParts = [];
 
 	const {
@@ -28,11 +34,19 @@ export function attributes(interaction: CommandInteraction) {
 		const enabledFlags = enabled.map((v) => formatFlag(v.name));
 		void redis.sadd(ATTRIBUTES(guildID), ...enabledFlags);
 		messageParts.push(CONFIG_ATTRIBUTES_ENABLED(enabledFlags.map((f) => `\`${f}\``).join(', ')));
-	}
-	if (disabled.size) {
+	} else if (disabled.size) {
 		const disabledFlags = disabled.map((v) => formatFlag(v.name));
 		void redis.sadd(ATTRIBUTES(guildID), ...disabledFlags);
 		messageParts.push(CONFIG_ATTRIBUTES_DISABLED(disabledFlags.map((f) => `\`${f}\``).join(', ')));
+	} else {
+		const attributes = await redis.smembers(ATTRIBUTES(guildID));
+
+		const flags = attributes.map((a) => `\`${a}\``);
+		if (flags.length) {
+			messageParts.push(CONFIG_SHOW_ATTRIBUTES(flags.join(', ')));
+		} else {
+			messageParts.push(CONFIG_SHOW_ATTRIBUTES_NONE);
+		}
 	}
 
 	void interaction.reply({
