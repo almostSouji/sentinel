5054e13ae4fd2a5765ee532ea74e84eb9a4e29a6
almostSouji <timoqueezle@gmail.com>
2021-06-23 01:12:46 +0200

    chore(client): remove obsolete explicit constructor

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,4 +1,4 @@
-import { ClientOptions, Client } from 'discord.js';
+import { Client } from 'discord.js';
 import * as Redis from 'ioredis';
 
 declare module 'discord.js' {
@@ -9,7 +9,4 @@ declare module 'discord.js' {
 
 export default class extends Client {
 	public readonly redis: Redis.Redis = new Redis.default(6379, 'redis');
-	public constructor(clientOptions: ClientOptions) {
-		super(clientOptions);
-	}
 }
