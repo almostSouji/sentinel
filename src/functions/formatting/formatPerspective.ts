f88e29a7ae4cb4d454ffb6f59f8d5d7f6442a8b2
almostSouji <timoqueezle@gmail.com>
2021-08-18 12:02:39 +0200

    feat(perspective): i18n verbose descriptions

--- src/functions/formatting/formatPerspective.ts
+++ src/functions/formatting/formatPerspective.ts
@@ -5,39 +5,69 @@ import { PerspectiveResult } from '../inspection/checkPerspective';
 import { AttributeScoreMapEntry, nsfwAtrributes, nytAttributes } from '../perspective';
 import { inlineCode } from '@discordjs/builders';
 
-function mapKeyToAdverb(key: string): string {
+function mapKeyToVerbose(key: string, locale: string): string {
 	switch (key) {
 		case 'TOXICITY':
-			return 'toxic';
+			return i18next.t('flags.verbose.toxicity', {
+				lng: locale,
+			});
 		case 'SEVERE_TOXICITY':
-			return 'severely toxic';
+			return i18next.t('flags.verbose.severe_toxicity', {
+				lng: locale,
+			});
 		case 'IDENTITY_ATTACK':
-			return "attacking someone's identity";
+			return i18next.t('flags.verbose.identity_attack', {
+				lng: locale,
+			});
 		case 'INSULT':
-			return 'insulting';
+			return i18next.t('flags.verbose.insult', {
+				lng: locale,
+			});
 		case 'PROFANITY':
-			return 'profane';
+			return i18next.t('flags.verbose.profanity', {
+				lng: locale,
+			});
 		case 'THREAT':
-			return 'threatening';
+			return i18next.t('flags.verbose.threat', {
+				lng: locale,
+			});
 		case 'SEXUALLY_EXPLICIT':
-			return 'sexually explicit';
+			return i18next.t('flags.verbose.sexually_explicit', {
+				lng: locale,
+			});
 		case 'FLIRTATION':
-			return 'flirtatious';
+			return i18next.t('flags.verbose.flirtation', {
+				lng: locale,
+			});
 		case 'ATTACK_ON_AUTHOR':
 		case 'ATTACK_ON_COMMENTER':
-			return 'attacking someone';
+			return i18next.t('flags.verbose.attack_on', {
+				lng: locale,
+			});
 		case 'INCOHERENT':
-			return 'incoherent';
+			return i18next.t('flags.verbose.incoherent', {
+				lng: locale,
+			});
 		case 'INFLAMMATORY':
-			return 'inflammatory';
+			return i18next.t('flags.verbose.inflammatory', {
+				lng: locale,
+			});
 		case 'LIKELY_TO_REJECT':
-			return 'rejected by New York Times moderators';
+			return i18next.t('flags.verbose.likely_to_reject', {
+				lng: locale,
+			});
 		case 'OBSCENE':
-			return 'obscene';
+			return i18next.t('flags.verbose.obscene', {
+				lng: locale,
+			});
 		case 'SPAM':
-			return 'spam';
+			return i18next.t('flags.verbose.spam', {
+				lng: locale,
+			});
 		case 'UNSUBSTANTIAL':
-			return 'unsubstantial';
+			return i18next.t('flags.verbose.unsubstantial', {
+				lng: locale,
+			});
 		default:
 			return inlineCode(key);
 	}
@@ -71,7 +101,7 @@ export function formatPerspectiveDetails(data: AttributeScoreMapEntry[], locale:
 			})}`,
 		);
 	disclaimers.push(
-		`${PREFIX_LOCKED} ${i18next.t('attributes.explain_forced', {
+		`${PREFIX_LOCKED} ${i18next.t('flags.attributes.explain_forced', {
 			lng: locale,
 		})}`,
 	);
@@ -84,7 +114,7 @@ export function formatPerspectiveShort(data: PerspectiveResult, locale: string):
 	return high.length
 		? high
 				.sort((a, b) => b.score.value - a.score.value)
-				.map((e) => `${LIST_BULLET} ${mapKeyToAdverb(e.key)}`)
+				.map((e) => `${LIST_BULLET} ${mapKeyToVerbose(e.key, locale)}`)
 				.join('\n')
 		: i18next.t('checks.format_perspective_none', {
 				lng: locale,
